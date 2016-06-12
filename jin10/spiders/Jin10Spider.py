33710213954fb5762b1bd8ef3e5602f066fbe962
tangxuyao <daulgas.tang@sachsen.cc>
2016-06-12 18:56:51 +0800

    Initial commit

---
+++ jin10/spiders/Jin10Spider.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import scrapy.spiders
+
+class Jin10Spider(scrapy.Spiders):
+    name = "jin10"
+    allowed_domains = ["www.jin10.com"]
+    start_urls = ["http://www.jin10.com"]
+
+    def __init__(self):
+        print("init")
+
+    def parse(self, response):
+        pass
