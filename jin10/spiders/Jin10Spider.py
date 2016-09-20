66b48c0114385cd5d8e5268ff659e2f717c85847
tangxuyao <daulgas.tang@sachsen.cc>
2016-09-20 17:22:42 +0800

    修改过滤代码。

--- jin10/spiders/Jin10Spider.py
+++ jin10/spiders/Jin10Spider.py
@@ -9,28 +9,57 @@ class Jin10Spider(scrapy.Spider):
     allowed_domains = ["www.jin10.com"]
     start_urls = ["http://www.jin10.com"]
 
-    def _buildItem(self, mid, content, type):
+    def parse(self, response):
+        for sel in response.xpath("//div[@class='newsline']"):
+            mid = self._getmid(sel)
+            item = self._getcontent1(mid, sel)
+            if item != None:
+                yield item
+                continue
+
+            item = self._getcontent2(mid, sel)
+            if item != None:
+                yield item
+                continue
+
+            print("*** 没有对应的规则 ***")
+
+    def _builditem(self, mid, content, type):
         joins = ''.join(content)
         item = Jin10Item()
         item['content'] = joins.encode('utf-8')
-        item['mid'] = int(''.join(mid))
+        item['mid'] = int(mid)
         item['type'] = type
         return item
 
-    def parse(self, response):
-        for sel in response.xpath("//div[@class='newsline']"):
-            mid = sel.xpath("./attribute::id").extract()
-            content = sel.xpath("./table/tr/td[@id]/text()").extract()
-            length = len(content)
+    def _getmid(self, sel):
+        """
+        :param sel:
+        :return: string
+        """
+        mid = sel.xpath("./attribute::id").extract()
+        return ''.join(mid)
 
-            if length > 0:
-                print("执行了一级")
-                item = self._buildItem(mid, content, 0)
-                yield item
+    def _getcontent1(self, mid, sel):
+        """
+        :param sel:
+        :return:
+        """
+        list = sel.xpath("./table/tr/td[@id]/text()").extract()
+        if len(list) == 0:
+            return None
 
-            content = sel.xpath("./table/tr/td[@id]/b/text()").extract()
-            length = len(content)
-            if length > 0:
-                print("执行了二级")
-                item = self._buildItem(mid, content, 0)
-                yield item
+        str = ''.join(list)
+        return self._builditem(mid, str, 0)
+
+    def _getcontent2(self, mid, sel):
+        """
+        :param sel:
+        :return:
+        """
+        list = sel.xpath("./table/tr/td[@id]/b/text()").extract()
+        if len(list) == 0:
+            return None
+
+        str = ''.join(list)
+        return self._builditem(mid, str, 0)
